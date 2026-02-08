9230f3642362a8344664e35f89a7c50d9c8796fe
sanant <sanant@azerconnect.az>
2026-02-08 15:31:02 +0400

    Refactored some code blocks

--- src/main/java/com/devconnect/app/enums/ProductType.java
+++ src/main/java/com/devconnect/app/enums/ProductType.java
@@ -1,15 +1,31 @@
 package com.devconnect.app.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @AllArgsConstructor
-@Getter
 @ToString
 public enum ProductType {
     NEW("New"),
     USED("Used");
 
     private final String label;
+
+    @JsonValue
+    public String getLabel() {
+        return label;
+    }
+
+    @JsonCreator
+    public static ProductType fromValue(String value) {
+        for (ProductType productType : ProductType.values()) {
+            if (productType.name().equalsIgnoreCase(value) || productType.label.equalsIgnoreCase(value)) {
+                return productType;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Invalid ProductType: %s", value));
+    }
 }
