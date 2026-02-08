3abceace79bb2475061adc1c895a5c4ae69cd25a
sanant <sanant@azerconnect.az>
2026-02-08 14:44:30 +0400

    Created ProductController

---
+++ src/main/java/com/devconnect/app/controllers/ProductController.java
@@ -0,0 +1,52 @@
+package com.devconnect.app.controllers;
+
+import com.devconnect.app.dtos.product.ProductCreateDto;
+import com.devconnect.app.dtos.product.ProductDto;
+import com.devconnect.app.dtos.product.ProductUpdateDto;
+import com.devconnect.app.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/products", version = "1.0")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> create(@Valid @RequestBody ProductCreateDto createDto) {
+        ProductDto productDto = productService.create(createDto);
+        return new ResponseEntity<>(productDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getById(@PathVariable Long id) {
+        ProductDto productDto = productService.getById(id);
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAll() {
+        List<ProductDto> productDtoList = productService.getAll();
+        return new ResponseEntity<>(productDtoList, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> create(@PathVariable Long id, @Valid @RequestBody ProductUpdateDto updateDto) {
+        ProductDto productDto = productService.update(id, updateDto);
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
