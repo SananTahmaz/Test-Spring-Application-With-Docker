0a864729f9fabdac5cc5a81a43eccdc1c264c55c
sanant <sanant@azerconnect.az>
2026-02-11 18:32:06 +0400

    Created CategoryMapper

---
+++ src/main/java/com/devconnect/app/mappers/CategoryMapper.java
@@ -0,0 +1,29 @@
+package com.devconnect.app.mappers;
+
+import com.devconnect.app.dtos.category.CategoryCreateDto;
+import com.devconnect.app.dtos.category.CategoryDto;
+import com.devconnect.app.dtos.category.CategoryUpdateDto;
+import com.devconnect.app.entities.Category;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    private final ModelMapper modelMapper;
+
+    public CategoryMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public CategoryDto toDto(Category category) {
+        return modelMapper.map(category, CategoryDto.class);
+    }
+
+    public Category toEntity(CategoryCreateDto createDto) {
+        return modelMapper.map(createDto, Category.class);
+    }
+
+    public void updateEntity(CategoryUpdateDto updateDto, Category category) {
+        modelMapper.map(updateDto, category);
+    }
+}
