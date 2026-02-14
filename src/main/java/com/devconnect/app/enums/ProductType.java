b873df770b5151d2b40589681714d65aa5552f4c
sanant <sanant@azerconnect.az>
2026-02-14 23:05:38 +0400

    Removed unused annotation

--- src/main/java/com/devconnect/app/enums/ProductType.java
+++ src/main/java/com/devconnect/app/enums/ProductType.java
@@ -3,7 +3,6 @@ package com.devconnect.app.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.ToString;
 
 @AllArgsConstructor
